2c8ff58dfa75b41ca39d9218ee0ca684543ff5ff
Wishyou2021 <95857756+Wishyou2021@users.noreply.github.com>
2021-12-09 20:31:09 +0800

    Update 654.py

--- 654.py
+++ 654.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-# w= float(input（'請輸入體重(KG)？“））
-W = st.number_input（‘請輸入體重(KG)？‘)
+# w= float(input（'請輸入體重(KG)？'））
+W = st.number_input（'請輸入體重(KG)？')
 h = st.number_input（'請輸入身高(M)？'）
 confirm_input = st.button( ‘輪人確認”）
 
